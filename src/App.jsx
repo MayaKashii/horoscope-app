742550a0a019ac92612da939d4c79ddc758ae1fa
MayaKashii <donttouchmymustosh@gmail.com>
2025-06-22 08:28:35 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HoroscopeService from './components/HoroscopeService';
+
+function App() {
+  return (
+    <div className="App">
+      <HoroscopeService />
+    </div>
+  );
+}
+
+export default App;
